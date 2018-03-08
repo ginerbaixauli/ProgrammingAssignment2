6723dc57faf5ecedafa67fdcc5915e002038074e
Carlos Giner Baixauli <ginerbaixauli@gmail.com>
2018-03-08 20:33:22 +0100

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation so it may be a good idea
+## to caching the inverse of a matrix rather than compute it repeatedly.
+## In this assignment we are going to write a pair of functions that cache 
+## the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function gets a matrix and creates a special matrix
+## object that can cache its inverse. It will return a list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Set inverse matrix to null
+  im <- NULL
+  
+  # Set original matrix
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  
+  # Get function and original matrix
+  get <- function() x
+  
+  # Set inverse matrix
+  setinv <- function(solve) im <<- solve
+  
+  # Get inverse matrix
+  getinv <- function() im
+  
+  # Include set, get, setinv and getinv into a list
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special matrix
+## returned by makeCacheMatrix above. If the inverse has already been
+## calculated and the matrix has not changed, then this function should
+## retrieve the inverse from the cache. If not, it will get the matrix
+## and compute the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Get inverse matrix
+  im <- x$getinv()
+  
+  # Check if inverse matrix is cached
+  if(!is.null(im)) {
+    message("getting cached inverse matrix")
+    
+    # If cached, return inverse matrix
+    return(m)
+  }
+  
+  # Else, get original matrix and compute inverse
+  mat <- x$get()
+  im <- solve(mat, ...)
+  
+  # Set inverse matrix in cache
+  x$setinv(im)
+  
+  # Return inverse matrix
+  im
 }
